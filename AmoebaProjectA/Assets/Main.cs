21cdddc7674398b60e8d2126e6dcba1fddd417ee
LeeSeok <salo3999@naver.com>
2016-10-04 01:10:56 +0900

    대망의 프로젝트 시작입니다.

---
+++ AmoebaProjectA/Assets/Main.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+    public UILabel label;
+	// Use this for initialization
+	void Start () 
+    {
+        label.text = "여러분 이제 시작합니다.";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
